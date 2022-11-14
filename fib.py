bb6764785df19deaea3a3ab329bac78c5fa3fe16
Light Ediand <45293474+lightediand@users.noreply.github.com>
2022-11-14 15:50:14 +0000

    Create fib.py

---
+++ fib.py
@@ -0,0 +1,16 @@
+natural_number = input("Please specify a natural number for the range: ")
+
+n = int(natural_number)
+
+i = 0
+j = 1
+
+with open("fibonacci.txt", "w") as f
+    
+    f.write("{}\n{}\n".format(i,j))
+
+    while i+j <= n:
+        z = i + j
+        f.write("{}\n".format(z))
+        i = j
+        j = z
